ddd42d8c96f329c9fb534d1008d0ed47da3c586b
sarahpai <sarahpai@github.com>
2018-09-30 22:24:35 -0400

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,53 @@
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  return rand(1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(card_total)
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  user_input = gets.chomp
+  return user_input
 end
 
-def end_game
-  # code #end_game here
+def end_game(card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  first_round = deal_card + deal_card
+  display_card_total(first_round)
+  return first_round
 end
 
-def hit?
-  # code hit? here
+def hit?(current_card)
+  prompt_user
+    user_input = get_user_input
+    if user_input == "h"
+      dealed_card = deal_card
+      current_card += dealed_card
+      return current_card
+    elsif user_input == "s"
+      return current_card
+    else
+      invalid_command
+      prompt_user
+      return current_card
+    end
 end
+
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
 end
 
 #####################################################
@@ -39,6 +55,17 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  first_round = initial_round
+  until first_round > 21 do
+    first_round = hit?(first_round)
+    display_card_total(first_round)
+  end
+  end_game(first_round)
 end
-    
+
+def initial_round
+  first_round = deal_card + deal_card
+  display_card_total(first_round)
+  return first_round
+end
--- lib/runner.rb
+++ lib/runner.rb
@@ -1,3 +1,12 @@
 require_relative "blackjack.rb"
 
-runner
+runner 
+def runner
+  welcome
+  first_round = initial_round
+  until initial_round > 21 do
+    first_round = hit?(initial_round)
+    display_card_total(initial_round)
+  end
+  end_game(initial_round)
+end
